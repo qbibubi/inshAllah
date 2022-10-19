abdd0bc302883d969c93db6350908a9cef081f7e
piasek <piaskowski3456@gmail.com>
2022-10-19 15:09:11 +0200

    pogchampik widzowie

--- app/src/main/java/com/example/photoedit/MainActivity.kt
+++ app/src/main/java/com/example/photoedit/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.photoedit
 
-import com.example.photoedit.R
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -13,25 +11,85 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // image view
-        val imageMain = findViewById<ImageView>(R.id.mainImage);
+        val imageMain = findViewById<ImageView>(R.id.mainImage)
 
         // rotation seek bars
-        val rotateX = findViewById<SeekBar>(R.id.rotationX);
-        val rotateY = findViewById<SeekBar>(R.id.rotationY);
+        val rotateX = findViewById<SeekBar>(R.id.rotationX)
+        val rotateY = findViewById<SeekBar>(R.id.rotationY)
 
-        rotateX?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        rotateX.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 imageMain.rotationX = p1.toFloat()
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
-                TODO("Not yet implemented")
+                // i love
             }
 
             override fun onStopTrackingTouch(p0: SeekBar?) {
-                TODO("Not yet implemented")
+                // code bro
             }
         })
 
+        rotateY.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                imageMain.rotationY = p1.toFloat()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                // i love
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                // code bro
+            }
+        })
+
+        // hue seek bars
+        val red = findViewById<SeekBar>(R.id.redFilterBar)
+        val green = findViewById<SeekBar>(R.id.greenFilterBar)
+        val blue = findViewById<SeekBar>(R.id.redFilterBar)
+
+        red.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                // change red hue
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                // i love
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                // code bro
+            }
+        })
+
+        green.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                // change green hue
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                // i love
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                // code bro
+            }
+        })
+
+        blue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                // change blue hue
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                // i love
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                // code bro
+            }
+        })
     }
 }
